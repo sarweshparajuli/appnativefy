8c694685480149d025819af8d5fc217cdfd5da03
Sarwesh Parajuli <60316871+sarweshparajuli@users.noreply.github.com>
2021-02-16 11:51:06 +0545

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 name=$1
 mkdir ~/AppImage-maker && cd ~/AppImage-maker && mkdir nativefier-appimage-temp
-mv "$name"-linux-* nativefier-appimage-temp/"$name".AppDir
+  mv "$name"-linux-x86_64 nativefier-appimage-temp/"$name".AppDir
+  mv "$name"-linux-x*64 nativefier-appimage-temp/"$name".AppDir
+
 cd nativefier-appimage-temp
 (
   
@@ -9,6 +11,7 @@ cd nativefier-appimage-temp
   
 iconname="icon.png"
 
+  
 echo
 
     FILE=resources/app/icon.png
@@ -19,8 +22,9 @@ else
     wget -c "https://raw.githubusercontent.com/sarweshparajuli/nativefier-appimage/main/icon.png" -O resources/app/icon.png
 fi   
 
-
+  
   cp resources/app/icon.png icon.png
+
   
   
   echo "[Desktop Entry]" > $name.desktop
@@ -39,11 +43,8 @@ fi
 [ ! -e /tmp/appimagetool ] && wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O /tmp/appimagetool
 chmod +x /tmp/appimagetool
 /tmp/appimagetool "$name.AppDir"
+
 cp *.AppImage ../
 cd ..
-echo
-echo
-echo "AppImage built to $PWD/$name-x86_64.AppImage"
-echo
-echo
-echo
+
+echo "AppImage built to ~/AppImage-maker/$name-x86_64.AppImage"
